0fe8a98881e1d26d8fe87c097f5359769b1ba5ce
Kasandra Orta <kasandraorta26@gmail.com>
2023-06-08 21:51:26 -0600

    Add more program

--- friends.js
+++ friends.js
@@ -1,6 +1,15 @@
 var myArray =[5, 13, 28, 45, 53, 64, 78, 81];
+
 for (i = 0; i < myArray.length; i++) {
     if (myArray[i] > 30){
     console.log(myArray[i])
     }
 }
+
+for (i = 0; i < myArray.length; i++) {
+    if (myArray[i] < 60){
+    console.log(myArray[i])
+    }
+}
+
+// Kasandra and Patrick are the greatest!
